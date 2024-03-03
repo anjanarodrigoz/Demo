ee8e9d3f9324c5d52053f64cddb6ee5ad324aead
anjanarodrigoz <85009096+anjanarodrigoz@users.noreply.github.com>
2024-03-03 20:30:42 +0530

    add stage to customer Form

---
+++ src/main/java/controller/DashBoardController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashBoardController {
+
+    public AnchorPane dashBoardPane;
+
+
+    public void customerBtnOnAction(ActionEvent actionEvent) {
+           Stage stage =  (Stage)dashBoardPane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/CustomerForm.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
